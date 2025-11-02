41cddb0ea8bb12bea64930b22d81c75656604f8f
caioxdev <caioseabradequeiroz2006@gmail.com>
2025-11-02 16:29:52 -0300

    Adicionando mais models

--- ifood/src/main/java/br.com.poo.ifood/model/Produto.java
+++ ifood/src/main/java/br.com.poo.ifood/model/Produto.java
@@ -1,4 +1,58 @@
 package br.com.poo.ifood.model;
 
 public class Produto {
+    private Integer id_produto;
+    private String descricao;
+    private Integer quantidade;
+    private Double preco;
+    private String nome;
+
+    public Produto() {}
+
+    public Produto (String descricao, Integer quantidade, Double preco, String nome) {
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.preco = preco;
+        this.nome = nome;
+    }
+
+    public Integer getId_produto() {
+        return id_produto;
+    }
+
+    public void setId_produto(Integer id_produto) {
+        this.id_produto = id_produto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
--- ifood/src/main/java/br.com.poo.ifood/model/Restaurante.java
+++ ifood/src/main/java/br.com.poo.ifood/model/Restaurante.java
@@ -1,4 +1,59 @@
 package br.com.poo.ifood.model;
 
 public class Restaurante {
+    private String nome;
+    private String telefone;
+    private String endereco;
+    private String categoria;
+    private Double avaliacao;
+
+    public Restaurante() {}
+
+    public Restaurante(String nome, String telefone, String endereco, String categoria, double avaliacao) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.categoria = categoria;
+        this.avaliacao = avaliacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
 }
