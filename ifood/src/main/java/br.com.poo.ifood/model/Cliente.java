2de2fc70882bb2d3270c7e32d5b02876acf5f2f3
caioxdev <caioseabradequeiroz2006@gmail.com>
2025-11-02 16:07:14 -0300

    Adicionando modals e fazendo modal cliente

---
+++ ifood/src/main/java/br.com.poo.ifood/model/Cliente.java
@@ -0,0 +1,49 @@
+package br.com.poo.ifood.model;
+
+public class Cliente {
+    private String nome;
+    private String email;
+    private String senha;
+    private String telefone;
+
+    public Cliente() {}
+
+    public Cliente(String nome, String email, String senha, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
---
+++ ifood/src/main/java/br.com.poo.ifood/model/Pedido.java
@@ -0,0 +1,4 @@
+package br.com.poo.ifood.model;
+
+public class Pedido {
+}
